6685a19e42f86b42ec76d5670a4e84184c97c505
Bartosz Dzionek <vburtoch@gmail.com>
2021-07-27 14:51:29 +0100

    Add model.R and generate_features.R

---
+++ generate_features.R
@@ -0,0 +1,82 @@
+library(tensorflow)  # requires also the Python Tensorflow
+library(keras)
+
+# Inception model without fully connected top layer, with max pooling
+# This is our 'feature generator'.
+# NB: I've occasionally had problems with loading this inception-v3 model
+#  possibly due to conflicting h5py version in python. Re-installing from above
+#  seems to fix it for the moment
+inception_v3_feature_model <- application_inception_v3(
+  include_top = FALSE,
+  weights = "imagenet",
+  input_tensor = NULL,
+  input_shape = NULL,
+  pooling = "max"
+)
+
+#' Given an image path, extract features for that image.
+#' 
+#' @param img_path The path of an individual image.
+#' @param top_trim The number of pixels that should be trimmed from the top.
+#' @param bottom_trim The number of pixels that should be trimmed from the bottom.
+#' @return The features of the image.
+#' 
+#' @note The input image format for this model is different than for the VGG16
+#'       and ResNet models (299x299 instead of 224x224).
+extract_features_inception_v3 <- function(img_path, top_trim, bottom_trim) {
+  # Load image as colour and resize
+  img <- image_load(
+    img_path,
+    grayscale = FALSE,
+    #target_size = c(299, 299),
+    #interpolation = "nearest"
+  )
+  
+  # Trim the info bar
+  img_crop <- defined_crop(img, top_trim = top_trim, bottom_trim = bottom_trim,
+                           min_width = 50, min_height = 50)
+  
+  # Resize the image to 299 x 299
+  resized_img <- image_array_resize(img_crop, width=299, height=299)
+  
+  # Reshape for preprocess function
+  resized_img <- array_reshape(resized_img, c(1, dim(resized_img)))
+  vals <- inception_v3_preprocess_input(resized_img)
+  
+  # pass image into inception model and get output features
+  # e.g. predicting to near the final layers and getting those values
+  feats <- inception_v3_feature_model %>% predict(vals)
+  feats
+}
+
+#' Get features from a directory of images.
+#' 
+#' @param directory_path The path to the base directory of images.
+#' @param top_trim The number of pixels that should be trimmed from the top.
+#' @param bottom_trim The number of pixels that should be trimmed from the bottom.
+#' @return The data frame of features associated with the given images.
+get_features <- function(directory_path, top_trim, bottom_trim, save = FALSE) {
+  files <- list.files(
+    directory_path, pattern = "*.JPG|*.jpg", full.names = TRUE
+  )
+  feature_list <- list()
+  
+  for (i in seq_along(files)) {
+    f <- files[i]
+    print(sprintf("[%d] Processing: %s", i, f))
+    feature_list[[f]] <- extract_features_inception_v3(f, top_trim, bottom_trim)
+  }
+  
+  # Convert to dataframe
+  features_df <- data.frame(matrix(
+    unlist(feature_list), nrow=length(feature_list), byrow=TRUE
+  ))
+  
+  if (save) {
+    # Store result as a file for this directory in the current working directory
+    fwrite(features_df, "_features_v3.csv")
+  }
+  
+  # Do not print the output (useful if just used to save features.)
+  invisible(features_df)
+}
